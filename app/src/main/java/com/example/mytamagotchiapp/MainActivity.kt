9c6bab17ffd7298b86f9ffdddb22d307f3e3d05c
MattGilau <ST10438081@vcconnect.edu.za>
2024-04-29 22:21:38 +0200

    First commit

---
+++ app/src/main/java/com/example/mytamagotchiapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.mytamagotchiapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Find the button with id 'btnStart' and assign it to variable btnStart
+        val btnStart = findViewById<Button>(R.id.btnStart)
+
+        // Lambda expression for the button function
+        btnStart.setOnClickListener{
+
+            /* Display a short duration pop-up msg to indicate the button is clicked and the
+               MainActivityTamagotchi is being started */
+            Toast.makeText(this, R.string.starting_message, Toast.LENGTH_SHORT).show()
+            // Externalise text string to string resources for good practise
+
+            //Explicit intent to specify the MainActivityTamagotchi class should be started
+            val intent = Intent(this, MainActivityTamagotchi::class.java)
+
+            // Start MainActivityTamagotchi using the intent created
+            startActivity(intent)
+
+            // Logs to provide warnings, debugging assistance and verbose comments
+            Log.w("MainActivity", "Starting of the second screen not " +
+                    "tested for exceptions.")
+            Log.d("MainActivity", "Button 'CLICK TO START!' clicked to navigate" +
+                    " to MainActivityTamagotchi.")
+            Log.v("Functionality", "A new screen was added per the specifications layed-" +
+                    "out for the assignment.")
+        }
+
+        Log.d("MainActivity", "onCreate method called when MainActivity initialised.")
+    }
+}
---
+++ app/src/main/java/com/example/mytamagotchiapp/MainActivityTamagotchi.kt
@@ -0,0 +1,11 @@
+package com.example.mytamagotchiapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityTamagotchi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_tamagotchi)
+    }
+}
